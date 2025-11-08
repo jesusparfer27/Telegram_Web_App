7e43e576a59cc642e4c7d34104a4998c1db1c9fb
jesusparfer27 <jesusparfer@alumnos.cei.es>
2025-11-08 17:15:36 +0100

    commit

--- Bot/bot.js
+++ Bot/bot.js
@@ -1,5 +1,5 @@
 const { Telegraf } = require("telegraf");
-const TOKEN = "5153026043:AAEOA6Jgze21I8PGuZWKJoNMGY1Wzl6S5OI";
+const TOKEN = "8559491752:AAFz6gFQ8fUs0CUFCrDHE3aPNwLN8IrZH_M";
 const bot = new Telegraf(TOKEN);
 
 const web_link = "https://celebrated-torte-184681.netlify.app/";
